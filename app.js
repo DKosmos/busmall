36c122b6cb24f036ffc17cfb0782f05852b12b5b
DKosmos <davidakosmos@gmail.com>
2017-05-22 18:49:05 -0700

    added initial html structure and app plans

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//create constructor for each product
+//create methods (find name from url, others)
+//create array of product objects
+//create array for participant answers
+//create functions that captures user responses, progresses through products, etc
+//create functions that tally the answers and presents the data
