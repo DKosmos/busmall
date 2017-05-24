cba27f40563e59dc16fe4b8cb27578cf9d13dc90
DKosmos <davidakosmos@gmail.com>
2017-05-24 14:33:54 -0700

    save working, but not finding current state

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var counter = 0;
+
 //create constructor for each product
 function Product(name, url) {
   this.name = name;
@@ -24,38 +24,57 @@ Product.prototype.beenChosen = function () {
   this.timesselected++;
 };
 
-//create array of product objects
-var bag = new Product('Bag', './img/bag.jpg');
-var banana = new Product('Banana', './img/banana.jpg');
-var bathroom = new Product('Bathroom', './img/bathroom.jpg');
-var boots = new Product('Boots', './img/boots.jpg');
-var breakfast = new Product('Breakfast', './img/breakfast.jpg');
-var bubblegum = new Product('Bubblegum', './img/bubblegum.jpg');
-var chair = new Product('Chair', './img/chair.jpg');
-var cthulhu = new Product('Cthulhu', './img/cthulhu.jpg');
-var dogduck = new Product('Dog-Duck', './img/dog-duck.jpg');
-var dragon = new Product('Dragon', './img/dragon.jpg');
-var pen = new Product('Pen', './img/pen.jpg');
-var petsweep = new Product('Pet-sweep', './img/pet-sweep.jpg');
-var scissors = new Product('Scissors', './img/scissors.jpg');
-var shark = new Product('Shark', './img/shark.jpg');
-var sweep = new Product('Sweep', './img/sweep.png');
-var tauntaun = new Product('Tauntaun', './img/tauntaun.jpg');
-var unicorn = new Product('Unicorn', './img/unicorn.jpg');
-var usb = new Product('USB', './img/usb.gif');
-var watercan = new Product('Water-can', './img/water-can.jpg');
-var wineglass = new Product('Wine-glass', './img/wine-glass.jpg');
+if (localStorage.list){
+  var counter = localStorage.counter;
+  var userSelectionHistory = JSON.parse(localStorage.userSelectionHistory);
+  var excludedNumbers = localStorage.excludedNumbers;
+  var productArray = JSON.parse(localStorage.productArray);
+} else {
+  var counter = 0;
+  var userSelectionHistory = [];
+  var excludedNumbers = [];
+  //create array of product objects
+  var bag = new Product('Bag', './img/bag.jpg');
+  var banana = new Product('Banana', './img/banana.jpg');
+  var bathroom = new Product('Bathroom', './img/bathroom.jpg');
+  var boots = new Product('Boots', './img/boots.jpg');
+  var breakfast = new Product('Breakfast', './img/breakfast.jpg');
+  var bubblegum = new Product('Bubblegum', './img/bubblegum.jpg');
+  var chair = new Product('Chair', './img/chair.jpg');
+  var cthulhu = new Product('Cthulhu', './img/cthulhu.jpg');
+  var dogduck = new Product('Dog-Duck', './img/dog-duck.jpg');
+  var dragon = new Product('Dragon', './img/dragon.jpg');
+  var pen = new Product('Pen', './img/pen.jpg');
+  var petsweep = new Product('Pet-sweep', './img/pet-sweep.jpg');
+  var scissors = new Product('Scissors', './img/scissors.jpg');
+  var shark = new Product('Shark', './img/shark.jpg');
+  var sweep = new Product('Sweep', './img/sweep.png');
+  var tauntaun = new Product('Tauntaun', './img/tauntaun.jpg');
+  var unicorn = new Product('Unicorn', './img/unicorn.jpg');
+  var usb = new Product('USB', './img/usb.gif');
+  var watercan = new Product('Water-can', './img/water-can.jpg');
+  var wineglass = new Product('Wine-glass', './img/wine-glass.jpg');
 
-var productArray = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogduck, dragon, pen, petsweep, scissors, shark, sweep, tauntaun, unicorn, usb, watercan, wineglass];
+  var productArray = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogduck, dragon, pen, petsweep, scissors, shark, sweep, tauntaun, unicorn, usb, watercan, wineglass];
+}
 
-//create array for participant answers
-var userSelectionHistory = [];
-var excludedNumbers = [];
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var canvas2 = document.getElementById('canvas2');
 var ctx2 = canvas2.getContext('2d');
 //create functions that captures user responses, progresses through products, etc
+
+function saveToLocalStorage() {
+  localStorage.counter = counter;
+  console.log(counter, localStorage.counter);
+  localStorage.excludedNumbers = excludedNumbers;
+  console.log(excludedNumbers, localStorage.excludedNumbers);
+  localStorage.productArray = JSON.stringify(productArray);
+  console.log(productArray, localStorage.productArray);
+  localStorage.userSelectionHistory = JSON.stringify(userSelectionHistory);
+  console.log(userSelectionHistory, localStorage.userSelectionHistory);
+}
+
 function generateRandom(){
   var arrayIndex = Math.floor(Math.random() * productArray.length);
   return arrayIndex;
@@ -117,6 +136,7 @@ function createEventListeners(){
       productSelected = productSelected.replace('-','');
       window[productSelected].pickedMe();
       logUserSelections();
+      saveToLocalStorage();
       updatedCounterHtml();
       userSelectionHistory.push(productSelected);
       logPreviousSelection();
