d97a18aede935261ca020a7c659e38cdfdb612b7
DKosmos <davidakosmos@gmail.com>
2017-05-24 16:35:32 -0700

    fixed state, except radar chart isn't working if refreshed at 25 selections

--- js/app.js
+++ js/app.js
@@ -25,10 +25,49 @@ Product.prototype.beenChosen = function () {
 };
 
 if (localStorage.productArray){
-  var counter = localStorage.counter;
+  var counter = parseInt(localStorage.counter);
   var userSelectionHistory = JSON.parse(localStorage.userSelectionHistory);
   var excludedNumbers = JSON.parse('[' + localStorage.excludedNumbers + ']');
-  var productArray = JSON.parse(localStorage.productArray);
+  var productArraySaved = JSON.parse(localStorage.productArray);
+  //create array of product objects
+  var bag = new Product('Bag', './img/bag.jpg');
+  var banana = new Product('Banana', './img/banana.jpg');
+  var bathroom = new Product('Bathroom', './img/bathroom.jpg');
+  var boots = new Product('Boots', './img/boots.jpg');
+  var breakfast = new Product('Breakfast', './img/breakfast.jpg');
+  var bubblegum = new Product('Bubblegum', './img/bubblegum.jpg');
+  var chair = new Product('Chair', './img/chair.jpg');
+  var cthulhu = new Product('Cthulhu', './img/cthulhu.jpg');
+  var dogduck = new Product('Dog-Duck', './img/dog-duck.jpg');
+  var dragon = new Product('Dragon', './img/dragon.jpg');
+  var pen = new Product('Pen', './img/pen.jpg');
+  var petsweep = new Product('Pet-sweep', './img/pet-sweep.jpg');
+  var scissors = new Product('Scissors', './img/scissors.jpg');
+  var shark = new Product('Shark', './img/shark.jpg');
+  var sweep = new Product('Sweep', './img/sweep.png');
+  var tauntaun = new Product('Tauntaun', './img/tauntaun.jpg');
+  var unicorn = new Product('Unicorn', './img/unicorn.jpg');
+  var usb = new Product('USB', './img/usb.gif');
+  var watercan = new Product('Water-can', './img/water-can.jpg');
+  var wineglass = new Product('Wine-glass', './img/wine-glass.jpg');
+
+  var productArray = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogduck, dragon, pen, petsweep, scissors, shark, sweep, tauntaun, unicorn, usb, watercan, wineglass];
+
+  for (var i=0; i<productArray.length; i++){
+    productArray[i].timesclicked = productArraySaved[i].timesclicked;
+    productArray[i].timesselected = productArraySaved[i].timesselected;
+  }
+
+  updatedCounterHtml();
+  generateSelectionIndexes();
+  placeImgTagsInDom();
+  logPreviousSelection();
+  if(counter<24){
+    createEventListeners();
+  } else{
+    createResultTags();
+    createdRadarChart();
+  }
 } else {
   var counter = 0;
   var userSelectionHistory = [];
@@ -56,6 +95,12 @@ if (localStorage.productArray){
   var wineglass = new Product('Wine-glass', './img/wine-glass.jpg');
 
   var productArray = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogduck, dragon, pen, petsweep, scissors, shark, sweep, tauntaun, unicorn, usb, watercan, wineglass];
+
+  updatedCounterHtml();
+  generateSelectionIndexes();
+  placeImgTagsInDom();
+  logPreviousSelection();
+  createEventListeners();
 }
 
 var canvas = document.getElementById('canvas');
@@ -249,9 +294,4 @@ function topFiveClicked(){
   topFiveArray = topFiveArray.splice(0,5);
   return topFiveArray;
 }
-
-generateSelectionIndexes();
-placeImgTagsInDom();
-logPreviousSelection();
-createEventListeners();
 //create functions that tally the answers and presents the data
